ce7bc5fb41a2a845a2cec750692449e26d84ae9e
gi098 <vyntqe180099@fpt.edu.vn>
2026-01-25 22:59:47 +0700

    Test: Verify addFirst correctly adds node to the head

--- travel_planner/src/test/java/com/travelplanner/structures/TourLinkedListTest.java
+++ travel_planner/src/test/java/com/travelplanner/structures/TourLinkedListTest.java
@@ -9,7 +9,23 @@ public class TourLinkedListTest {
     private TourLocation createLoc(String id, String name) {
         return new TourLocation(id, name, "Description", 100.0);
     }
+     @Test
+    public void testAddLast() {
+        TourLinkedList list = new TourLinkedList();
+        list.addLast(createLoc("1", "A"));
+        list.addLast(createLoc("2", "B"));
+        assertEquals("A -> B -> END", list.print());
+    }
+     @Test
+    public void testAddFirst() {
+        TourLinkedList list = new TourLinkedList();
+        list.addLast(createLoc("1", "A"));
+        list.addFirst(createLoc("2", "B")); // Thêm B vào trước A
+        assertEquals("B -> A -> END", list.print());
+    }
 }
+
+
 
    
    
