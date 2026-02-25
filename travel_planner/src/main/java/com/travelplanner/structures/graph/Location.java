9311d9a2b22ac63b43c05627eee90356ec112d9e
gi098 <gi098@users.noreply.github.com>
2026-02-25 20:57:29 +0700

    feat(graph): add Location model class

---
+++ travel_planner/src/main/java/com/travelplanner/structures/graph/Location.java
@@ -0,0 +1,24 @@
+package com.travelplanner.structures.graph;
+
+public class Location {
+    private String id;
+    private String name;
+
+    public Location(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + id + ")";
+    }
+}
